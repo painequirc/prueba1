f81eb8dbcd500d276d679e580279cec106be9b4f
Gonzalo Painequir <gonzalo.painequir@gmail.com>
2015-05-22 15:31:19 -0300

    new

---
+++ Prueba1/src/Prueba.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * @author jpaineq
+ *
+ */
+public class Prueba {
+
+	/**
+	 * 
+	 */
+	public Prueba() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
